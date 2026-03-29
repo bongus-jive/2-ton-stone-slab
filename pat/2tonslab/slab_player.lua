099431fe851d40a4e97b27188d4c238975ffa138
patmanf <27874300+patmanf@users.noreply.github.com>
2026-03-29 14:31:04 -0500

    tab tuah

--- pat/2tonslab/slab_player.lua
+++ pat/2tonslab/slab_player.lua
@@ -1,35 +1,35 @@
 function init()
-	status.clearPersistentEffects("die")
-	
-	slabPane()
-	message.setHandler("pat_2tonslab_pane", slabPane)
+  status.clearPersistentEffects("die")
+  
+  slabPane()
+  message.setHandler("pat_2tonslab_pane", slabPane)
 
-	message.setHandler("pat_2tonslab", function(_, isLocal, a)
-		if isLocal then
-			if a == "spawn" then
-				local m = mcontroller.position()
-				local y = math.min(m[2] + 150, world.size()[2] - 1)
-				
-				world.spawnProjectile("pat_2tonslab", {m[1], y}, player.id(), {0,0}, false, {myNuts = math.huge})
-				
-			elseif a == "kill" then
-				status.applySelfDamageRequest({
-					damage = status.resourceMax("health"),
-					damageType = "IgnoresDef",
-					damageSourceKind = "hammer",
-					sourceEntityId = player.id()
-				})
-				status.addEphemeralEffect("pat_2tonslab_effect")
-			end
-		end
-	end)
+  message.setHandler("pat_2tonslab", function(_, isLocal, a)
+    if isLocal then
+      if a == "spawn" then
+        local m = mcontroller.position()
+        local y = math.min(m[2] + 150, world.size()[2] - 1)
+        
+        world.spawnProjectile("pat_2tonslab", {m[1], y}, player.id(), {0,0}, false, {myNuts = math.huge})
+        
+      elseif a == "kill" then
+        status.applySelfDamageRequest({
+          damage = status.resourceMax("health"),
+          damageType = "IgnoresDef",
+          damageSourceKind = "hammer",
+          sourceEntityId = player.id()
+        })
+        status.addEphemeralEffect("pat_2tonslab_effect")
+      end
+    end
+  end)
 end
 
 function slabPane()
-	local uuid = sb.makeUuid()
-	math.pat_2tonslab_uuid = uuid
-	
-	pane = root.assetJson("/pat/2tonslab/window/slab.sussy")
-	pane.uuid = uuid
-	player.interact("ScriptPane", pane)
+  local uuid = sb.makeUuid()
+  math.pat_2tonslab_uuid = uuid
+  
+  pane = root.assetJson("/pat/2tonslab/window/slab.sussy")
+  pane.uuid = uuid
+  player.interact("ScriptPane", pane)
 end
--- pat/2tonslab/window/slab_window.lua
+++ pat/2tonslab/window/slab_window.lua
@@ -1,49 +1,49 @@
 function init()
-	uuid = config.getParameter("uuid")
-	
-	canvYes = {c = widget.bindCanvas("yes"), data = "spawn"}
-	canvNo = {c = widget.bindCanvas("no")}
-	
-	function canvYes:draw(p) draw(self.c, "yes", p) end
-	function canvNo:draw(p) draw(self.c, "no", p) end
-	
-	canvYes:draw()
-	canvNo:draw()
+  uuid = config.getParameter("uuid")
+  
+  canvYes = {c = widget.bindCanvas("yes"), data = "spawn"}
+  canvNo = {c = widget.bindCanvas("no")}
+  
+  function canvYes:draw(p) draw(self.c, "yes", p) end
+  function canvNo:draw(p) draw(self.c, "no", p) end
+  
+  canvYes:draw()
+  canvNo:draw()
 end
 
 function update(dt)
-	if uuid ~= math.pat_2tonslab_uuid then
-		pane.dismiss()
-	end
+  if uuid ~= math.pat_2tonslab_uuid then
+    pane.dismiss()
+  end
 end
 
 function draw(canvas, image, a)
-	canvas:clear()
-	canvas:drawImage("/pat/2tonslab/window/buttons.png:"..image..(a and "_push" or ""), {0, 0}, 0.2)
+  canvas:clear()
+  canvas:drawImage("/pat/2tonslab/window/buttons.png:"..image..(a and "_push" or ""), {0, 0}, 0.2)
 end
 
 function click(canvas, position, button, isButtonDown)
-	if button ~= 0 then return end
-	
-	local size = canvas.c:size()
-	local inButton = position[1] > 0 and position[1] <= size[1] and position[2] > 0 and position[2] <= size[2]
-	
-	if isButtonDown and inButton then
-		pane.playSound("/pat/2tonslab/window/click.ogg", 0, 0.75)
-	end
-	
-	if not isButtonDown or inButton then
-		canvas:draw(isButtonDown)
-		
-		if not isButtonDown and canvas.lastButtonDown and inButton then
-			if canvas.data then
-				world.sendEntityMessage(player.id(), "pat_2tonslab", canvas.data)
-			end
-			pane.dismiss()
-		end
-		
-		canvas.lastButtonDown = isButtonDown
-	end
+  if button ~= 0 then return end
+  
+  local size = canvas.c:size()
+  local inButton = position[1] > 0 and position[1] <= size[1] and position[2] > 0 and position[2] <= size[2]
+  
+  if isButtonDown and inButton then
+    pane.playSound("/pat/2tonslab/window/click.ogg", 0, 0.75)
+  end
+  
+  if not isButtonDown or inButton then
+    canvas:draw(isButtonDown)
+    
+    if not isButtonDown and canvas.lastButtonDown and inButton then
+      if canvas.data then
+        world.sendEntityMessage(player.id(), "pat_2tonslab", canvas.data)
+      end
+      pane.dismiss()
+    end
+    
+    canvas.lastButtonDown = isButtonDown
+  end
 end
 
 function clickYes(...) click(canvYes, ...) end
